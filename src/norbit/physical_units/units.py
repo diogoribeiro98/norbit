a2c39a6d6fa96f1847cc4a589c195e701b501046
Diogo Ribeiro <diogo.gnc.ribeiro@gmail.com>
2024-07-09 13:56:31 +0200

    Update units to match conventions

--- src/norbit/physical_units/units.py
+++ src/norbit/physical_units/units.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 #Constants of nature
-c = 3e8
-G = 6.67e-11
+c = 299792458 #m/s
+G = 6.67430e-11 #m/kg/s^2
 
 #prefixes
 deci  = 1e-1
@@ -22,29 +22,29 @@ hour    = 60*minute
 day     = 24*hour 
 week    = 7*day
 month   = 30*day
-year    = 365*day
+year    = 365.25*day
 
 #Lenght units
 meter = 1
 kilometer           = 1e4*meter
 
-parsec              = 3.086e16*meter
+parsec              = 3.08567758149e16*meter
 kiloparsec          = kilo*parsec
 megaparsec          = mega*parsec
 
-astronomical_unit   = 1.496e11
+astronomical_unit   = 149597870700*meter
 
 #Mass units
-kilogram   = 1
-solar_mass = 2e30*kilogram
+kilogram   = 1.0
+solar_mass = 1.98841e30*kilogram
 
 #Angular conversion factors
 rad_to_as = (180/np.pi)*60*60
-as_to_rad = 1/rad_to_as
-deg_to_rad = np.pi/180
-rad_to_deg = 1/deg_to_rad
+as_to_rad = 1.0/rad_to_as
+deg_to_rad = np.pi/180.0
+rad_to_deg = 1.0/deg_to_rad
 
 #galactic center units
-Mbh = 4.297e6*solar_mass
-R0  = 1.7e9*astronomical_unit
+Mbh = 4.29701742727e6*solar_mass
+R0  = 8277.09055007*parsec
 Rg  = G*Mbh/c**2 
