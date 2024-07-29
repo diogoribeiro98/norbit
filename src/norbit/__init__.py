ce92c835cf5cc3315f9ffc1930f8682a12a92533
Diogo Ribeiro <diogo.gnc.ribeiro@gmail.com>
2024-07-29 23:52:47 +0200

    explicit package imports

--- src/norbit/__init__.py
+++ src/norbit/__init__.py
@@ -2,3 +2,15 @@
 
 __author__ = """dribeiro"""
 __version__ = '0.1.0'
+
+
+
+#Post Newtonian tools
+
+from .pnutils import kepler_period
+from .pnutils import get_angular_momentum_vector
+from .pnutils import get_apocenter_position_and_velocity
+from .pnutils import get_apocenter_unit_vectors
+
+from .pnutils import get_pericenter_position_and_velocity
+from .pnutils import get_pericenter_unit_vectors
